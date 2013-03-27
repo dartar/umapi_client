779107666a8471119ce7ca56ce940576c88e3117
rfaulk <rfaulkner@wikimedia.org>
2013-03-27 02:28:13 -0700

    add. test stubs.

---
+++ umapi_client/test/tests.py
@@ -0,0 +1,26 @@
+"""
+Implement tests for umapi_client.
+"""
+
+def test_good_request_no_params_raw():
+    assert False  # TODO: implement your test here
+
+def test_good_request_no_params_agg():
+    assert False  # TODO: implement your test here
+
+def test_good_request_no_params_ts():
+    assert False  # TODO: implement your test here
+
+def test_good_request_with_params_raw():
+    assert False  # TODO: implement your test here
+
+def test_good_request_with_params_agg():
+    assert False  # TODO: implement your test here
+
+def test_good_request_with_params_ts():
+    assert False  # TODO: implement your test here
+
+def test_bad_request():
+    assert False  # TODO: implement your test here
+
+
