74528b6143a55d3da023b9ca2f601ca20c0423fa
rfaulk <rfaulkner@wikimedia.org>
2013-04-01 01:59:21 -0700

    mod. refs to cookie data relative to cookie path.

--- umapi_client/client.py
+++ umapi_client/client.py
@@ -7,7 +7,7 @@ __email__ = 'rfaulkner@wikimedia.org'
 __date__ = "2013-03-03"
 __license__ = "GPL (version 2 or later)"
 
-from umapi_client import config, logging
+from umapi_client import config
 
 import json
 import cookielib
@@ -26,8 +26,9 @@ class UMAPIClient(object):
         self.login = login
         self.password = password
 
-        self.cj = cookielib.MozillaCookieJar(config.COOKIE_FILENAME)
-        if os.access(config.COOKIE_FILENAME, os.F_OK):
+        self.cj = cookielib.MozillaCookieJar(config.COOKIE_DIR +
+                                             config.COOKIE_FILENAME)
+        if os.access(config.COOKIE_DIR + config.COOKIE_FILENAME, os.F_OK):
             self.cj.load()
         self.opener = urllib2.build_opener(
             urllib2.HTTPRedirectHandler(),
